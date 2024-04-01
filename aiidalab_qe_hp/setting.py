4d5fe371d0e435d9d8991de28bae733033103b28
superstar54 <xingwang1991@gmail.com>
2024-04-01 09:06:36 +0000

    add QeappHpWorkChain

--- aiidalab_qe_hp/setting.py
+++ aiidalab_qe_hp/setting.py
@@ -8,6 +8,7 @@ from aiida.orm import StructureData
 
 from aiidalab_qe.common.panel import Panel
 
+
 class Setting(Panel):
     title = "HP Settings"
     identifier = "hp"
@@ -22,7 +23,7 @@ class Setting(Panel):
         """<div style="padding-top: 0px; padding-bottom: 0px">
         <h4>Hubbard V</h4></div>"""
     )
-    
+
     def __init__(self, **kwargs):
         # method to calculate the Hubbard U and V values: One-shot or Self-consistent
         self.method = ipw.Dropdown(
@@ -48,7 +49,7 @@ class Setting(Panel):
             disabled=False,
             style={"description_width": "initial"},
         )
-        
+
         self.hubbard_u = ipw.VBox()
         self.hubbard_v = ipw.VBox()
         self.qpoints_distance = ipw.FloatText(
@@ -67,7 +68,7 @@ class Setting(Panel):
             indent=False,
             value=True,
         )
-        
+
         self.calculation_type.observe(self._generate_hubbard_table, names="value")
 
         self.children = [
@@ -88,11 +89,13 @@ class Setting(Panel):
         """Return a dictionary with the input parameters for the plugin."""
         hubbard_u = [
             [data[1].value, data[2].value, data[3].value]
-            for data in self.hubbard_u_map.values() if data[0].value
+            for data in self.hubbard_u_map.values()
+            if data[0].value
         ]
         hubbard_v = [
             [data[1].value, data[2].value, data[3].value, data[4].value, data[5].value]
-            for data in self.hubbard_v_map.values() if data[0].value
+            for data in self.hubbard_v_map.values()
+            if data[0].value
         ]
         parameters = {
             "method": self.method.value,
@@ -120,10 +123,12 @@ class Setting(Panel):
             checkbox.value = True
             manifold.value = data[1]
             U.value = data[2]
-        
+
         hubbard_v = input_dict.get("hubbard_v", [])
         for data in hubbard_v:
-            checkbox, _, manifold1, _, manifold2, V = self.hubbard_v_map[(data[0], data[2])]
+            checkbox, _, manifold1, _, manifold2, V = self.hubbard_v_map[
+                (data[0], data[2])
+            ]
             checkbox.value = True
             manifold1.value = data[1]
             manifold2.value = data[3]
@@ -132,11 +137,10 @@ class Setting(Panel):
     @tl.observe("input_structure")
     def _update_structure(self, _=None):
         self._generate_hubbard_table()
-    
+
     def _generate_hubbard_table(self, _=None):
         self._generate_u()
         self._generate_v()
-        
 
     def _generate_u(self):
         """Generate a table with the U values for the selected structure.
@@ -150,10 +154,15 @@ class Setting(Panel):
         rows = []
         self.hubbard_u_map = {}
         for kind_symbol in kind_list:
-            checkbox = ipw.Checkbox(value=False, description='', indent=False, layout=ipw.Layout(width='20px'))
+            checkbox = ipw.Checkbox(
+                value=False,
+                description="",
+                indent=False,
+                layout=ipw.Layout(width="20px"),
+            )
             symbol = ipw.HTML(value=kind_symbol)
-            manifold = ipw.Text(value='', placeholder='', description='Manifold')
-            u = ipw.FloatText(value=1e-10, description='U', min=1e10, max=100)
+            manifold = ipw.Text(value="", placeholder="", description="Manifold")
+            u = ipw.FloatText(value=1e-10, description="U", min=1e10, max=100)
             rows.append(ipw.HBox([checkbox, symbol, manifold, u]))
             self.hubbard_u_map[kind_symbol] = [checkbox, symbol, manifold, u]
         self.hubbard_u.children = rows
@@ -177,14 +186,28 @@ class Setting(Panel):
             for j in range(i + 1, len(kind_list)):
                 kind_symbol1 = kind_list[i]
                 kind_symbol2 = kind_list[j]
-                checkbox = ipw.Checkbox(value=False, description='', indent=False, layout=ipw.Layout(width='20px'))
+                checkbox = ipw.Checkbox(
+                    value=False,
+                    description="",
+                    indent=False,
+                    layout=ipw.Layout(width="20px"),
+                )
                 symbol1 = ipw.HTML(value=kind_symbol1)
-                manifold1 = ipw.Text(value='', placeholder = '', description='Manifold')
+                manifold1 = ipw.Text(value="", placeholder="", description="Manifold")
                 symbol2 = ipw.HTML(value=kind_symbol2)
-                manifold2 = ipw.Text(value='', placeholder = '', description='Manifold')
-                v = ipw.FloatText(value=1e-10, description='V', min=1e10, max=100)
-                rows.append(ipw.HBox([checkbox, symbol1, manifold1, symbol2, manifold2, v]))
-                self.hubbard_v_map[(kind_symbol1, kind_symbol2)] = [checkbox, symbol1, manifold1, symbol2, manifold2, v]
+                manifold2 = ipw.Text(value="", placeholder="", description="Manifold")
+                v = ipw.FloatText(value=1e-10, description="V", min=1e10, max=100)
+                rows.append(
+                    ipw.HBox([checkbox, symbol1, manifold1, symbol2, manifold2, v])
+                )
+                self.hubbard_v_map[(kind_symbol1, kind_symbol2)] = [
+                    checkbox,
+                    symbol1,
+                    manifold1,
+                    symbol2,
+                    manifold2,
+                    v,
+                ]
         print("hubbard_v_map: ", self.hubbard_v_map)
         self.hubbard_v.children = rows
 
@@ -192,4 +215,3 @@ class Setting(Panel):
         """Reset the panel to its initial state."""
         self.input_structure = None
         self.hubbard_u.children = []
-        
