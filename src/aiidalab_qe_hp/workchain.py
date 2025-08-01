f7894f68d32067735fcc5fcca8ef9be0e787e105
AndresOrtegaGuerrero <oandresg15@gmail.com>
2025-08-01 07:28:10 +0000

    SelfConsistentHubbardWorkChain from aiida_hubbard

--- src/aiidalab_qe_hp/workchain.py
+++ src/aiidalab_qe_hp/workchain.py
@@ -1,6 +1,6 @@
 from aiida_quantumespresso.data.hubbard_structure import HubbardStructureData
 from aiida_quantumespresso.common.types import ElectronicType, SpinType
-from aiida_quantumespresso_hp.workflows.hubbard import SelfConsistentHubbardWorkChain
+from aiida_hubbard.workflows.hubbard import SelfConsistentHubbardWorkChain
 from aiida import orm
 from aiidalab_qe.utils import (
     enable_pencil_decomposition,
